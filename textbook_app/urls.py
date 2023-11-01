6a384175f4e6d5e675ee09750c59d37fdd793329
Kroleski <rkroleski@comcast.net>
2023-11-01 08:26:22 -0600

    Cleanup .gitignore

--- textbook_app/urls.py
+++ textbook_app/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-#path function defines a url pattern
-#'' is empty to represent based path to app
-# views.index is the function defined in views.py
-# name='index' parameter is to dynamically create url
-# example in html <a href="{% url 'index' %}">Home</a>.
 path('', views.index, name='index'),
+#path('book/', views.BookListView.as_view(), name= 'book'),
+#path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+#path('book/<int:pk>/delete/', views.deleteBook, name='book-delete'),
+#path('book/<int:pk>/update/', views.updateBook, name='book-update'),
+path('users/', views.UserListView.as_view(), name= 'users'),
+path('user/<int:pk>', views.UserDetailView.as_view(), name='user-detail'),
+#path('user/<int:pk>/create_book/', views.createBook, name='book-create'),
 ]
--- textbook_app/views.py
+++ textbook_app/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from .forms import BookForm
+from .models import User
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
+from django.views import generic
+from typing import Any
+
 def index(request):
 
 # Render the HTML template index.html with the data in the context variable.
    return render( request, 'textbook_app/index.html')
+
+class UserListView(generic.ListView):
+    model = User
+    
+class UserDetailView(generic.DetailView):
+    model = User
