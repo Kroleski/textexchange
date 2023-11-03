5c00ecd2b2f060516fce68b47ed7a4e6a8b5948d
Kroleski <rkroleski@comcast.net>
2023-11-03 11:42:22 -0600

    Fixed Typo in Migration

--- django_project/settings.py
+++ django_project/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'textbook_app',
-    'bootstrap5',
 ]
 
 AUTHENTICATION_BACKENDS = [
@@ -70,9 +69,6 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
-            'builtins':[
-                'bootstrap5.templatetags.bootstrap5',
-            ]
         },
     },
 ]
